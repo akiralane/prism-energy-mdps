706f658b699b348812255b14a1405290cae3fbfc
Akira Lane <ajl1g20@soton.ac.uk>
2023-02-25 18:16:17 +0000

    Completed environment-case merge algorithm (untested)

--- prism/src/explicit/ExtentsExplicit.java
+++ prism/src/explicit/ExtentsExplicit.java
@@ -37,11 +37,20 @@ public class ExtentsExplicit extends Extents implements IExtents {
      * Computes the result of merging the extents from the given state's successors (as described by
      * {@code merge} in the progress report). Requires the model for player and transition information.
      * Note that this doesn't (shouldn't) mutate the "extents" object.
-     * <p>
-     * <em>Player case:</em> for each key (energy value) in the successors:<ul>
+     * <p><p></p>
+     * <em>Environment case: </em>Weight the probabilities, then for each key (energy value) in the combined successors:
+     * <ul>
+     *      <li>Sum over the associated weighted probabilities in the successors for that key</li>
+     *      <li>If the energy value doesn't exist, then take the next lowest energy value's probability (which can be thought
+     *      of as the "current" probability of that extent)</li>
+     *      <li>Put (energy, sum) in computed extent</li>
+     * </ul>
+     * <em>Player case:</em> For each lifted energy value in the combined successors:
+     * <ul>
      *    <li>Get the list of probabilities in the successors associated with that key and pick the highest</li>
-     *    <li>If the highest value in list lower than highest value in the extent so far: omit key from computed extent</li>
-     *    <li>Otherwise: put (key, max(list)) in computed extent</li>
+     *    <li>If the highest value in the list is lower than highest value in the extent so far, omit that key from computed extent
+     *    since there's no probability improvement. I don't think this should ever happen, but...</li>
+     *    <li>Otherwise: put (energy, max(list)) in computed extent</li>
      * </ul>
      * </p>
      * @return The extent computed as a result of the merge.
@@ -51,76 +60,99 @@ public class ExtentsExplicit extends Extents implements IExtents {
         var resultExtent = new TreeMap<Double, Double>();
         var transitions = emdp.getTransitions(stateIndex);
 
-        if (emdp.getEnvironmentPlayer() == emdp.getPlayer(stateIndex)) { // probabilistic
-            /*
-            (look at your notebook! keep track of the "routes" somehow and use those)
-            whenever we encounter an entry, update the "routes" of the successor extents which participate
-            take the weighted sum of the values on the routes
-            */
+        // --------------------- Environment-state merge ---------------------
+        if (emdp.getEnvironmentPlayer() == emdp.getPlayer(stateIndex)) {
 
             var energyValues = new TreeSet<Double>();
             var successorExtents = new HashSet<TreeMap<Double, Double>>();
-            var routes = new HashMap<Integer, Double>();
 
-            // for each successor state, lift and store its extent, add its keys (energy values) to a set
+            // 1. for each successor state, weight and store its extent, add its keys (energy values) to a set
             for (Map.Entry<Integer, TransitionWeight> transition : transitions) {
 
                 var targetState = transition.getKey();
-                var probability = transition.getValue().value();
-
-                if (transition.getValue().type().equals(TransitionWeight.Type.Energy)) {
+                var weight      = transition.getValue();
+                if (weight.type().equals(TransitionWeight.Type.Energy)) {
                     throw new PrismException(
                             "Found energy transition \""+
-                                    stateIndex+" --("+probability+")-> "+targetState+
-                                    "\" from Environment state while merging extents!"
+                            stateIndex+" --("+weight.value()+")-> "+targetState+
+                            "\" from Environment state while merging extents!"
                     );
                 }
 
-                var targetExtent = extents.get(targetState);
-                successorExtents.add(targetExtent);
-                energyValues.addAll(targetExtent.keySet());
-
-                // TODO continue
-
+                // 1.1 weight extent by the probability of its transition
+                var oldExtent = extents.get(targetState);
+                var weightedExtent = new TreeMap<Double, Double>();
+                for (Map.Entry<Double, Double> entry : oldExtent.entrySet()) {
+                    var energy      = entry.getKey();
+                    var probability = entry.getValue();
+                    weightedExtent.put(
+                        energy,                      // original energy
+                        probability * weight.value() // weighted probability
+                    );
+                }
+                successorExtents.add(weightedExtent);
+                energyValues.addAll(weightedExtent.keySet());
             }
 
-        } else { // controller's choice
+            // 2. now create the corresponding output entry for each energy value
+            for (Double energy : energyValues) {
+
+                // 2.1 get a list of probability values for this energy
+                var probValues = new ArrayList<Double>();
+                for (var extent : successorExtents) {
+                    // get the energy value, or the next lowest if it doesn't exist
+                    // see notebook for the reasoning behind this
+                    var entry = extent.floorEntry(energy);
+                    var probability = (entry != null) ? entry.getValue() : 0;
+                    probValues.add(probability);
+                }
+
+                // 2.2 sum them
+                var weightedSum = probValues.stream().reduce(0.0, Double::sum);
+                resultExtent.put(energy, weightedSum);
+            }
+
+        // --------------------- Controller-state merge ---------------------
+        } else {
 
             // TODO incorporate tracking states for strategies
 
             var energyValues = new TreeSet<Double>();
             var successorExtents = new HashSet<TreeMap<Double, Double>>();
 
-            // for each successor state, lift and store its extent, add its keys (energy values) to a set
+            // 1. for each successor state, lift and store its extent, add its keys (energy values) to a set
             for (Map.Entry<Integer, TransitionWeight> transition : transitions) {
 
                 var targetState = transition.getKey();
-                var cost = transition.getValue().value();
-
-                if (transition.getValue().type().equals(TransitionWeight.Type.Probabilistic)) {
+                var weight      = transition.getValue();
+                if (weight.type().equals(TransitionWeight.Type.Probabilistic)) {
                     throw new PrismException(
                             "Found probabilistic transition \""+
-                                    stateIndex+" --("+cost+")-> "+targetState+
-                                    "\" from Controller state while merging extents!"
+                            stateIndex+" --("+weight.value()+")-> "+targetState+
+                            "\" from Controller state while merging extents!"
                     );
                 }
 
-                // "lift" extent by the cost of the transition
+                // 1.1. "lift" extent by the cost of the transition
                 var oldExtent = extents.get(targetState);
                 var liftedExtent = new TreeMap<Double, Double>();
-                for (Double energy : oldExtent.keySet()) {
-                    liftedExtent.put(energy + cost, oldExtent.get(energy));
+                for (Map.Entry<Double, Double> entry : oldExtent.entrySet()) {
+                    var energy      = entry.getKey();
+                    var probability = entry.getValue();
+                    liftedExtent.put(
+                            energy + weight.value(), // lifted cost
+                            probability              // original probability
+                    );
                 }
-
                 successorExtents.add(liftedExtent);
                 energyValues.addAll(liftedExtent.keySet());
             }
 
-            // create the corresponding output entry for each energy value
+            // 2. now create the corresponding output entry for each energy value that we want
             double highestProbInExtent = 0.0;
             for (Double energy : energyValues) {
 
-                // find the highest probability in the successors associated with this energy
+                // 2.1 find the highest probability in the successors associated with this energy
                 double highestProbForThisEnergy = 0.0;
                 for (TreeMap<Double, Double> extent : successorExtents) {
                     if (extent.containsKey(energy)) {
@@ -128,13 +160,13 @@ public class ExtentsExplicit extends Extents implements IExtents {
                     }
                 }
 
-                // if it's higher than the current highest probability in the extent,
+                // 2.2 if it's higher than the current highest probability in the extent,
                 // update the new highest and put (energy, value) in the output
                 if (highestProbForThisEnergy > highestProbInExtent) {
                     highestProbInExtent = highestProbForThisEnergy;
                     resultExtent.put(energy, highestProbForThisEnergy);
                 }
-                // otherwise, don't include this energy value in the output, since it's redundant
+                // otherwise, don't bother including this energy value in the output, since it's redundant
             }
         }
 
